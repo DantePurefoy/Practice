edb30a8458d0852043a3436b480c67e97eb8c925
Dante Purefoy <47311488+DDDDDante@users.noreply.github.com>
2020-06-26 17:14:41 -0700

    while into for 0-100-0-100-0 with no duplicates

---
+++ 0to100to0to0/0to100to0to0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _0to100to0to0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 100)
+            {
+                Console.WriteLine("{0} w", i);
+                i++;
+            }
+
+            while (i >= 0)
+            {
+                
+                Console.WriteLine("{0} w", i);
+                i--;
+
+            }
+
+            for(i=1; i <= 100; i++)
+            {
+                Console.WriteLine("{0} f", i);
+                
+            }
+
+            for (i = 99; i >= 0; i--)
+            {
+                Console.WriteLine("{0} f", i);
+
+            }
+
+        }
+    }
+}
