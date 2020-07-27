d11a3e695ab36f97435175dd50bf31b8c9143a82
Dante Purefoy <47311488+DDDDDante@users.noreply.github.com>
2020-07-27 16:57:26 -0700

    Pluralsight_Gradebook_Practice

---
+++ Pluralsight_Gradebook/Pluralsight_Gradebook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluralsight_Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int students;
+            double result = 0.0;
+
+            List<double> grades = new List<double>() { };
+
+           
+
+            Console.WriteLine("Enter the number of students in the class");
+            students = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("There are {0} students in the class", students);
+
+            foreach (double number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N2}");
+
+        }
+    }
+}
